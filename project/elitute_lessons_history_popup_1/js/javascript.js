2457d00274c6c3e39fd9c324e0ecc64fc546ebdf
Alexandr Pyrkin <kesandr803@gmail.com>
2014-07-18 22:20:13 +0400

    First version

---
+++ project/elitute_lessons_history_popup_1/js/javascript.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function(){
+
+        $('.bg-overlay-dark').hide();
+        $('#dialog').hide();
+
+    var carouselContainers = $('#dialog, .bg-overlay-dark');
+    var onCarouselHiding = function() {
+        $('.bg-overlay-dark').hide();
+        $('#dialog').hide();
+        carouselPositionIndex = 0;
+        enableClosing = false;
+        $('#dialog').removeClass('open');
+    };
+
+
+
+    $('.c-open-poup').click(function(){
+     if ($('#dialog').hasClass('open')){
+      onCarouselHiding(); 
+      $('#dialog').removeClass('open')
+
+      return false;
+    }
+      $('#dialog').addClass('open');
+        carouselContainers.show();
+        $('.c-open-poup').addClass('active');
+    });
+
+
+    $('.c-close-popup').click(onCarouselHiding);
+    
+    $(document).on('keydown', function(e){
+            if (e.keyCode == 27) {
+            onCarouselHiding();
+        }
+    })
+})
